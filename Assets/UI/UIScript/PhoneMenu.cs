3a63c032b8bc30faf39b5f75054ba2bb203be91c
unknown <christianburberry@gmail.com>
2024-01-28 01:48:04 +0900

    Phone Menu Prefab

---
+++ Assets/UI/UIScript/PhoneMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneMenu : MonoBehaviour
+{
+    public Slider MusicSlider;
+    public Slider SoundEffectSlider;
+    public Toggle AutoToggle;
+    public Button MainMenuButton;
+
+    public void OnClickMenuButton()
+    {
+        Debug.Log("TODO - OnClickMenuButton");
+    }
+}
